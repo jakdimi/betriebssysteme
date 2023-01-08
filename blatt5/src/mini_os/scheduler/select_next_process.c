26e802dd6bab01551f98f01950c31a4fca9362b1
Steffen Weerts <steffen.weerts@gmail.com>
2023-01-08 11:44:04 +0100

    finished task 8

--- blatt5/src/mini_os/scheduler/select_next_process.c
+++ blatt5/src/mini_os/scheduler/select_next_process.c
@@ -1,17 +1,7 @@
 #include "../mini_os.h"
+#include <stdio.h>
 
 struct process *select_next_process(void)
 {
-	struct process *sel;
-
-	sel = ptable.run_queue.head;
-	/*
-	for (struct process *cur = sel->next; cur ; cur = cur->next) {
-		if (cur->process_id > sel->process_id) {
-			sel = cur;
-		}
-	}
-	*/
-	queue_remove(sel);
-	return sel;
+	return queue_pop(&ptable.run_queue);
 }
