69935e70fc6d7ce013d3e2d175bb0951692cd971
jakdimi <47759595+jakdimi@users.noreply.github.com>
2023-01-09 18:28:13 +0100

    add animation :D

--- blatt5/nikolausaufgabe/nikolausaufgabe.c
+++ blatt5/nikolausaufgabe/nikolausaufgabe.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h> 
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
 
-int main()
+int printSteffen()
 {
   FILE *fileptr;
   char c;
   char filename[] = "banner.txt";
+  int delay = 50;
   fileptr = fopen(filename, "r");
   if (fileptr == NULL)
   {
@@ -17,9 +23,22 @@ int main()
   while (c != EOF)
   {
     printf ("%c", c);
+    #ifdef _WIN32
+    Sleep(delay);
+    #else
+    usleep(delay);
+    #endif
     c = fgetc(fileptr);
   }
   
   fclose(fileptr);
   return 0;
+}
+
+int main()
+{
+  while(1)
+  {
+    printSteffen();
+  }
 }
